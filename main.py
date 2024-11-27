4f47be2565e5fdc04601a817766f64889bae49fc
Oleksandr <oleksandrkachura9@gmail.com>
2024-11-27 15:39:41 +0200

    write 'find common symbols' function

--- main.py
+++ main.py
@@ -22,3 +22,14 @@ def normalize_symbols(symbol: str) -> str:
         return symbol.replace("-", "/").replace("_", "/")
 
 
+#find common symbols
+def find_common_symbols(tickers_1: dict, tickers_2: dict) -> set:
+    """
+    :param tickers_1: the ticker data of exchange 1
+    :param tickers_2:
+    :return: a set of common symbols between the two exchanges
+    """
+    symbols_1 = set(map(normalize_symbols, tickers_1.keys()))
+    symbols_2 = set(map(normalize_symbols, tickers_2.keys()))
+    common_symbols = symbols_1 & symbols_2
+    return common_symbols
