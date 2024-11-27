2c27ded0924d288972e4d947dee45bc23ebfc2b6
Oleksandr <oleksandrkachura9@gmail.com>
2024-11-27 14:18:05 +0200

    write the 'normalize_symbols' function

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ import time
 mexc = ccxt.mexc()
 gateio = ccxt.gateio()
 
+
+#normalize all symbols
+def normalize_symbols(symbol: str) -> str:
+    """
+
+    :param symbol: the trading pair symbol
+    :return: the normalized symbol in the format of "BTC/USDT"
+    """
+
+    if symbol[-4:] in ["USDT", "USDC"] and "_" not in symbol\
+        and "-" not in symbol and "/" not in symbol:
+        new_symbol = symbol[:-4] + "/" + symbol[-4:]
+        return new_symbol
+    else:
+        return symbol.replace("-", "/").replace("_", "/")
+
+
